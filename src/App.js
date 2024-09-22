99f7dc0c4d1b564117923b506f403a46a3d0f477
Seemab Ahmed <seemab40615@gmail.com>
2024-09-22 10:37:01 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import ProgramSection from './components/ProgramSection';
+// import ProgramSection from './components/ProgramSection';
+import ProgramSection from './components/ProgramSection_update_One';
 // import ProgramSection from './components/TImeLine';
 
 function App() {
